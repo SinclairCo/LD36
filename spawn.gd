07ab15bfa3b0264bce3c60d6ca5b7dab3037ab29
Biliogadafr <biliogadafr@gmail.com>
2016-08-28 17:59:50 +0300

    Spawn

---
+++ spawn.gd
@@ -0,0 +1,18 @@
+
+extends Position2D
+
+var monster_class = preload("res://monster.tscn")
+var wait_time = 4.0;
+var wait_time_decrease = 0.1;
+
+func _ready():
+	get_node("timer").set_wait_time(wait_time * randf() * 10)
+	pass
+
+func _on_timer_timeout():
+	if(wait_time > 0.5):
+		wait_time -= wait_time_decrease
+	get_node("timer").set_wait_time((wait_time-wait_time_decrease) + randf() * 3)
+	var monster = monster_class.instance()
+	monster.set_global_pos(get_global_pos())
+	get_tree().get_current_scene().add_child(monster)

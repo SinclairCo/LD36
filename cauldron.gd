3205504827a132b3f593d9d803b93523b51ac50a
Biliogadafr <biliogadafr@gmail.com>
2016-08-29 23:05:37 +0300

    Cauldron music!

--- cauldron.gd
+++ cauldron.gd
@@ -4,12 +4,19 @@ extends Area2D
 export var need_to_collect = 50
 var collected = 0
 
+var player
+var cauldron_voice
+
 func _ready():
 	set_fixed_process(true)
+	player = get_node("player")
+	cauldron_voice = player.play("boiling")
 	pass
 
 func _fixed_process(delta):
 	var bodies = get_overlapping_bodies();
+	if(!player.is_voice_active(cauldron_voice)):
+		cauldron_voice = player.play("boiling")#don't know how to loop
 	for body in bodies:
 		var groups = body.get_groups()
 		if(groups.has("corpse_score")):
@@ -25,3 +32,8 @@ func _fixed_process(delta):
 		elif(body.has_method("on_damage")):
 			body.on_damage(1)
 			#print("whoops!")
+
+
+func _on_cauldron_body_enter( body ):
+	player.play("into_the_cauldron")
+	pass 
